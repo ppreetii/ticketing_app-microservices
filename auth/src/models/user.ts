cea4e6f12ee10a5767410795f2c535c172891eb2
ppreetii <ppreeti.guptaa@gmail.com>
2023-06-02 02:30:30 +0530

    hashing password

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,37 +1,48 @@
 import mongoose from "mongoose";
 
+import { Password } from "../services/password";
+
 //interface describing properties needed to create a user
 interface UserAttrs {
-    email:string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 //interface describing properties of User Model
 interface UserModel extends mongoose.Model<UserDoc> {
-    build(attrs: UserAttrs): UserDoc;
+  build(attrs: UserAttrs): UserDoc;
 }
 
 //interface describing properties that a User document has, one that is returned by Mongo
 interface UserDoc extends mongoose.Document {
-    email:string;
-    password:string;
+  email: string;
+  password: string;
 }
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-userSchema.statics.build = (attrs: UserAttrs) =>{
-    return new User(attrs);
-}
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
 
-const User = mongoose.model<UserDoc,UserModel>('User', userSchema);
+  done();
+});
 
-export {User};
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
