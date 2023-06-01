b6bd1a23bf7da23c2f81b584319e40593bc91a68
ppreetii <ppreeti.guptaa@gmail.com>
2023-06-01 20:38:12 +0530

    defining user document properties

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,12 +1,20 @@
 import mongoose from "mongoose";
 
+//interface describing properties needed to create a user
 interface UserAttrs {
     email:string;
     password: string;
 }
 
-interface UserModel extends mongoose.Model<any> {
-    build(attrs: UserAttrs): any;
+//interface describing properties of User Model
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc;
+}
+
+//interface describing properties that a User document has, one that is returned by Mongo
+interface UserDoc extends mongoose.Document {
+    email:string;
+    password:string;
 }
 
 const userSchema = new mongoose.Schema({
@@ -24,6 +32,6 @@ userSchema.statics.build = (attrs: UserAttrs) =>{
     return new User(attrs);
 }
 
-const User = mongoose.model<any,UserModel>('User', userSchema);
+const User = mongoose.model<UserDoc,UserModel>('User', userSchema);
 
 export {User};
