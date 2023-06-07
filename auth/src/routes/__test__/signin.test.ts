b72638b55b8db32fe957edf777344b7439cf8b5d
ppreetii <ppreeti.guptaa@gmail.com>
2023-06-08 00:03:27 +0530

    SignIn Functionality Testcases

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,50 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("Fails for Non-Existing Email", async () => {
+  return request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("Fails for Incorrect Password", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "wrong password",
+    })
+    .expect(400);
+});
+
+it("Respond with cookie for valid credentials", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  const response = await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(200);
+
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
