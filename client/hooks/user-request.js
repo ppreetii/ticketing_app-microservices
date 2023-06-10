cef459a1057072ffe12fa50b9896f7f20ed64460
ppreetii <ppreeti.guptaa@gmail.com>
2023-06-11 04:39:08 +0530

    adding signup form

---
+++ client/hooks/user-request.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { useState } from "react";
+
+export default ({ url, method, body }) => {
+  const [errors, setErrors] = useState(null);
+
+  const doRequest = async () => {
+    try {
+      setErrors(null);
+      const response = await axios[method](url, body);
+      return response.body;
+    } catch (error) {
+      setErrors(
+        <div className="alert alert-danger">
+          <h4>Error:</h4>
+          <ul className="my-0">
+            {error.response.data.errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  };
+
+  return {
+    doRequest,
+    errors,
+  };
+};
