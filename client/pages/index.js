6ff946ddfd61d95bc23069941b0089263a4b6d9f
ppreetii <ppreeti.guptaa@gmail.com>
2023-06-12 03:29:11 +0530

    creating reusable api client and finishing signup implementation

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,31 +1,17 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return <h1>Landing page</h1>;
+  return currentUser ? (
+    <h1>You are Signed In</h1>
+  ) : (
+    <h1>You are NOT Signed In</h1>
+  );
 };
 
-LandingPage.getInitialProps = async () => {
-  if (typeof window === "undefined") {
-    //we are on server
-    const { data } = await axios.get(
-      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-      {
-        headers: {
-          Host: "ticketing.com",
-        },
-      }
-    );
-
-    return data;
-  } else {
-    //we are on browser
-    const { data } = await axios.get("/api/users/currentuser");
-
-    return data;
-  }
-
-  return {};
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+  return data;
 };
 
 export default LandingPage;
