5ea6e358545c4fdd35e7c36755559089bc9cd333
ppreetii <ppreeti.guptaa@gmail.com>
2023-06-12 03:20:46 +0530

    fetching data during server side rendering

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,31 @@
-export default () =>{
-   return <h1>Landing page</h1>
-}
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  return <h1>Landing page</h1>;
+};
+
+LandingPage.getInitialProps = async () => {
+  if (typeof window === "undefined") {
+    //we are on server
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: {
+          Host: "ticketing.com",
+        },
+      }
+    );
+
+    return data;
+  } else {
+    //we are on browser
+    const { data } = await axios.get("/api/users/currentuser");
+
+    return data;
+  }
+
+  return {};
+};
+
+export default LandingPage;
