9aa557c4335fbf68e3303e9c0e9556608568e674
ppreetii <ppreeti.guptaa@gmail.com>
2023-06-16 04:00:41 +0530

    tickets service project setup and k8s configuration

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,9 +6,9 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY is not defined.");
   }
-   if (!process.env.MONGO_URI) {
-     throw new Error("MONGO_URI is not defined.");
-   }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI is not defined.");
+  }
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to database: ", process.env.MONGO_URI);
