242dfcb98043e91dad6935b012645f5dfcb053ab
ppreetii <ppreeti.guptaa@gmail.com>
2023-06-17 04:30:52 +0530

    ticket model implementation, create ticket route implementation, and added test case for same

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,4 +1,6 @@
 import request from "supertest";
+
+import { Ticket } from "../../models/ticket";
 import { app } from "../../app";
 
 it("Route handler listening to /api/tickets for post requests", async () => {
@@ -12,7 +14,10 @@ it("Can only be accessed if user is signed in", async () => {
 });
 
 it("Return a status other than 401 if user is signed in", async () => {
-  const response = await request(app).post("/api/tickets").set('Cookie', global.signin()).send({});
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({});
   expect(response.status).not.toEqual(401);
 });
 
@@ -21,15 +26,15 @@ it("Returns error if invalid title is provided", async () => {
     .post("/api/tickets")
     .set("Cookie", global.signin())
     .send({
-      title: '',
-      price: 10
+      title: "",
+      price: 10,
     })
     .expect(400);
   await request(app)
     .post("/api/tickets")
     .set("Cookie", global.signin())
     .send({
-      price: 10
+      price: 10,
     })
     .expect(400);
 });
@@ -47,9 +52,28 @@ it("Returns error if invalid price is provided", async () => {
     .post("/api/tickets")
     .set("Cookie", global.signin())
     .send({
-      title: "test"
+      title: "test",
     })
     .expect(400);
 });
 
-it("Creates ticket with valid input", async () => {});
+it("Creates ticket with valid input", async () => {
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
+  const title = "test",
+    price = 10;
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title,
+      price,
+    })
+    .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].title).toEqual(title);
+  expect(tickets[0].price).toEqual(price);
+});
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import { requireAuth,validateRequest } from "@preeti097/common";
 import { body } from "express-validator";
 
+import { Ticket } from "../models/ticket";
+
 const router = express.Router();
 
 router.post(
@@ -14,7 +16,18 @@ router.post(
       .withMessage("Price must be greater than 0."),
   ],
   validateRequest,
-  async (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const {title, price} = req.body;
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id
+    });
+
+    await ticket.save();
+
+    res.status(201).send(ticket);
+  }
 );
 
 export { router as createTicketRouter };
