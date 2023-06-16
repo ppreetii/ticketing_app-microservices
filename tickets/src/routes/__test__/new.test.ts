e7f19d6ec859c02c6210187963bec6ffa46ada82
ppreetii <ppreeti.guptaa@gmail.com>
2023-06-17 04:13:10 +0530

    writing test case before implementation, fake authentication for test, added test cases

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -12,12 +12,44 @@ it("Can only be accessed if user is signed in", async () => {
 });
 
 it("Return a status other than 401 if user is signed in", async () => {
-  const response = await request(app).post("/api/tickets").send({});
+  const response = await request(app).post("/api/tickets").set('Cookie', global.signin()).send({});
   expect(response.status).not.toEqual(401);
 });
 
-it("Returns error if invalid title is provided", async () => {});
+it("Returns error if invalid title is provided", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: '',
+      price: 10
+    })
+    .expect(400);
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      price: 10
+    })
+    .expect(400);
+});
 
-it("Returns error if invalid price is provided", async () => {});
+it("Returns error if invalid price is provided", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "test",
+      price: -10,
+    })
+    .expect(400);
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "test"
+    })
+    .expect(400);
+});
 
 it("Creates ticket with valid input", async () => {});
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,11 +1,12 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import request from "supertest";
+import jwt from "jsonwebtoken";
 
 import { app } from "../app";
 
 declare global {
-  var signin: () => Promise<string[]>;
+  var signin: () => string[];
 }
 
 let mongo: any;
@@ -31,16 +32,19 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
-  const email = "test@test.com";
-  const password = "password";
+global.signin = () => {
+  const token = jwt.sign(
+    {
+      id: new mongoose.Types.ObjectId().toHexString(),
+      email: "test@test.com",
+    },
+    process.env.JWT_KEY!
+  );
 
-  const response = await request(app)
-    .post("/api/users/signup")
-    .send({ email, password })
-    .expect(201);
+  const sessionJSON = JSON.stringify({
+    jwt: token,
+  });
 
-  const cookie = response.get("Set-Cookie");
-
-  return cookie;
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+  return [`session=${base64}`];
 };
