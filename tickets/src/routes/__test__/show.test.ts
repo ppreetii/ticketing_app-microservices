d4fdfbe2fd64b01d9da46709e09dc6f8fc088c34
ppreetii <ppreeti.guptaa@gmail.com>
2023-06-17 23:12:59 +0530

    added test cases for GET ticket by id endpoint

---
+++ tickets/src/routes/__test__/show.test.ts
@@ -0,0 +1,28 @@
+import request from "supertest";
+import mongoose from "mongoose";
+
+import { app } from "../../app";
+
+it('return 404 if ticket not found', async ()=>{
+    const id = new mongoose.Types.ObjectId().toHexString();
+    await request(app).get(`/api/tickets/${id}`).send().expect(404);
+});
+
+it('return 200 with ticket information if ticket exists', async ()=>{
+    const title = 'test', price = 10;
+    const res =  await request(app)
+       .post("/api/tickets")
+       .set("Cookie", global.signin())
+       .send({
+         title,
+         price,
+       })
+       .expect(201);
+    
+    const response = await request(app)
+      .get(`/api/tickets/${res.body.id}`)
+      .send()
+      .expect(200);
+    expect(response.body.title).toEqual(title);
+    expect(response.body.price).toEqual(price);
+});
