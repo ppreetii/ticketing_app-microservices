31443c8cfc4e9403d3318c6160d88940a54dc85a
ppreetii <ppreeti.guptaa@gmail.com>
2023-06-21 01:31:51 +0530

    concurrency control with durable subscription

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,29 +1,37 @@
-import nats,{Message} from "node-nats-streaming";
+import nats, { Message } from "node-nats-streaming";
 import { randomBytes } from "crypto";
 
-const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
-    url: "http://localhost:4222"
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
+  url: "http://localhost:4222",
 });
 
-stan.on("connect", ()=>{
-    console.log("Listener connected to NATS");
-    stan.on('close', ()=>{
-         console.log("Listener connection to NATS - CLOSED");
-         process.exit();
-    })
-    const options = stan.subscriptionOptions().setManualAckMode(true);
-    const subscription = stan.subscribe("ticket:created",'listenerQueueGroup' ,options);
+stan.on("connect", () => {
+  console.log("Listener connected to NATS");
+  stan.on("close", () => {
+    console.log("Listener connection to NATS - CLOSED");
+    process.exit();
+  });
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName("nats-listener-service");
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "listenerQueueGroup",
+    options
+  );
 
-    subscription.on("message", (msg: Message)=>{
-        const data = msg.getData();
-        if(typeof data === 'string'){
-            console.log(`
-            Received event #${msg.getSequence()}, with data: ${data}`)
-        }
+  subscription.on("message", (msg: Message) => {
+    const data = msg.getData();
+    if (typeof data === "string") {
+      console.log(`
+            Received event #${msg.getSequence()}, with data: ${data}`);
+    }
 
-        msg.ack();
-    })
+    msg.ack();
+  });
 });
 
-process.on('SIGINT', ()=> stan.close());
-process.on('SIGTERM', ()=> stan.close());
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
