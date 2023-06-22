1599ab12548a0f36c7b7ac54d274bd2517154936
ppreetii <ppreeti.guptaa@gmail.com>
2023-06-23 01:13:06 +0530

    publishing ticket creation

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,7 @@ import { requireAuth,validateRequest } from "@preeti097/common";
 import { body } from "express-validator";
 
 import { Ticket } from "../models/ticket";
+import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
 
 const router = express.Router();
 
@@ -25,7 +26,12 @@ router.post(
     });
 
     await ticket.save();
-
+    new TicketCreatedPublisher(client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId
+    })
     res.status(201).send(ticket);
   }
 );
