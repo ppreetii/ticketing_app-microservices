e2d421ae8cd777556a9a03ceab949f4cc590176f
ppreetii <ppreeti.guptaa@gmail.com>
2023-06-23 01:06:21 +0530

    updating common module with events implementation

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,4 +1,4 @@
-// Re-export stuff from errors and middlewares
+// Re-export stuff from errors, middlewares, events
 export * from "./errors/bad-request-error";
 export * from "./errors/custom-error";
 export * from "./errors/database-connection-error";
@@ -10,3 +10,9 @@ export * from "./middlewares/current-user";
 export * from "./middlewares/error-handler";
 export * from "./middlewares/require-auth";
 export * from "./middlewares/validate-request";
+
+export * from "./events/base-listener";
+export * from "./events/base-publisher";
+export * from "./events/subjects";
+export * from "./events/tickets/ticket-created-event";
+export * from "./events/tickets/ticket-updated-event";
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,19 +1,17 @@
 import nats from "node-nats-streaming";
 
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
+
 const stan = nats.connect('ticketing', 'abc', {
     url: "http://localhost:4222"
 });
 
-stan.on("connect", ()=>{
+stan.on("connect",async ()=>{
     console.log("Publisher connected to NATS");
-
-    const data = JSON.stringify({
-        id: "123",
-        title: "Concert",
-        price: 20
-    });
-
-    stan.publish("ticket:created", data, ()=>{
-        console.log("Event Published")
-    });
+    const publisher = new TicketCreatedPublisher(stan);
+    await publisher.publish({
+      id: '123',
+      title: "Ticket Event Test",
+      price: 100
+    })
 });
