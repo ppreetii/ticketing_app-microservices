530c9364c700da71fd88b31cd0beb6ec5696c6ae
ppreetii <ppreeti.guptaa@gmail.com>
2023-06-22 21:55:21 +0530

    using custom publisher

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -8,7 +8,7 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent>{
     readonly subject: Subjects.TicketCreated  = Subjects.TicketCreated;
     queueGroupName: string = "payments-service";
 
-    onMessage(data: any, msg: Message): void {
+    onMessage(data: TicketCreatedEvent['data'], msg: Message): void {
         console.log("Ticket Created Event data:", data);
         msg.ack();
     }
