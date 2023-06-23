bee11695705cbf8103cac4ea6b409de8cae9e691
ppreetii <ppreeti.guptaa@gmail.com>
2023-06-24 01:59:22 +0530

    graceful shutdown of nats and ticket update publishing

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -4,6 +4,7 @@ import { body } from "express-validator";
 
 import { Ticket } from "../models/ticket";
 import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -26,12 +27,12 @@ router.post(
     });
 
     await ticket.save();
-    // new TicketCreatedPublisher(client).publish({
-    //   id: ticket.id,
-    //   title: ticket.title,
-    //   price: ticket.price,
-    //   userId: ticket.userId
-    // })
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId
+    });
     res.status(201).send(ticket);
   }
 );
