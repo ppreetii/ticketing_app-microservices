a9a1f19fcd82ec867ddcdb446f71120834d16e1a
ppreetii <ppreeti.guptaa@gmail.com>
2023-06-28 00:59:19 +0530

    soft delete order logic implementation with test cases

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,72 @@
+import request from "supertest";
+import mongoose from "mongoose";
+
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+import { Order, OrderStatus } from "../../models/order";
+
+it("API can only be accessed if user is signed in", async () => {
+  const orderId = new mongoose.Types.ObjectId();
+  await request(app).delete(`/api/orders/${orderId}`).send().expect(401);
+});
+
+it("Validation Error for Invalid OrderId", async () => {
+  await request(app)
+    .get(`/api/orders/1234`)
+    .set("Cookie", global.signin())
+    .send()
+    .expect(400);
+});
+
+it("Marks Order as Cancelled for signed-in user", async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: "test",
+    price: 123,
+  });
+  await ticket.save();
+
+  //create an order for above ticket with signin user #1
+  const signedinUser = global.signin();
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", signedinUser)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  //cancel order of signin user #1
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set("Cookie", signedinUser)
+    .send()
+    .expect(204);
+
+  //get order and check its status as cancelled
+  const updatedOrder = await Order.findById(order.id);
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it("Fails when one use tries to cancel order of another user", async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: "test",
+    price: 123,
+  });
+  await ticket.save();
+
+  //create an order for above ticket with signin user #1
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  //signin user #2 cancels order of user #1
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set("Cookie", global.signin())
+    .send()
+    .expect(401);
+});
+
+it.todo("Emits events on order cancellation");
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,35 @@
 import express, { Request, Response } from "express";
+import {
+  requireAuth,
+  NotFoundError,
+  NotAuthorizedError,
+  validateRequest,
+} from "@preeti097/common";
+import { param } from "express-validator";
+import mongoose from "mongoose";
+
+import { Order,OrderStatus } from "../models/order";
 
 const router = express.Router();
 
-router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  [
+    param("orderId")
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("Invalid orderId"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId);
+    if (!order) throw new NotFoundError();
+    if (order.userId !== req.currentUser!.id) throw new NotAuthorizedError();
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
