da45ecf0632f6bd25527e7fd56303134e80fddf9
ppreetii <ppreeti.guptaa@gmail.com>
2023-07-13 23:18:13 +0530

    payment service initial setup

---
+++ payments/src/test/setup.ts
@@ -0,0 +1,50 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+declare global {
+  var signin: () => string[];
+}
+
+let mongo: any;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = "secret";
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+  await mongoose.connect(mongoUri, {});
+});
+
+jest.mock("../nats-wrapper");
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  if (mongo) {
+    await mongo.stop();
+  } 
+});
+
+global.signin = () => {
+  const token = jwt.sign(
+    {
+      id: new mongoose.Types.ObjectId().toHexString(),
+      email: "test@test.com",
+    },
+    process.env.JWT_KEY!
+  );
+
+  const sessionJSON = JSON.stringify({
+    jwt: token,
+  });
+
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+  return [`session=${base64}`];
+};
