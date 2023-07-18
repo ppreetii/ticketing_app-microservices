910d3c1bd42cd8dff00bbbc7142c75b20abdf884
ppreetii <ppreeti.guptaa@gmail.com>
2023-07-19 03:20:03 +0530

    updating common module with payment service event,and updating the common module in all other services

--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -4,4 +4,5 @@ export enum Subjects {
   OrderCreated = "order:created",
   OrderCancelled = "order:cancelled",
   ExpirationCompleted = "expiration:complete",
+  PaymentCreated = "payment:created"
 }
--- common/src/index.ts
+++ common/src/index.ts
@@ -20,3 +20,4 @@ export * from "./events/types/order-status";
 export * from "./events/orders/order-created-event";
 export * from "./events/orders/order-cancelled-event";
 export * from "./events/expiration/expiration-complete-event";
+export * from "./events/payments/payment-created-event";
