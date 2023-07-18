f66ac809a75398dad72e36ea1cb9bba7d0a8ef34
ppreetii <ppreeti.guptaa@gmail.com>
2023-07-19 03:36:31 +0530

    publishing payment created event

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -2,58 +2,66 @@ import mongoose from "mongoose";
 import request from "supertest";
 import { OrderStatus } from "@preeti097/common";
 
-import {app} from "../../app";
+import { app } from "../../app";
 import { Order } from "../../models/order";
 import { stripe } from "../../stripe";
 import { Payment } from "../../models/payment";
+import { natsWrapper } from "../../nats-wrapper";
 
-const url = '/api/payments';
+const url = "/api/payments";
 
-it("Return 404 if order does not exist", async ()=>{
-    await request(app).post(url).set("Cookie", global.signin()).send({
+it("Return 404 if order does not exist", async () => {
+  await request(app)
+    .post(url)
+    .set("Cookie", global.signin())
+    .send({
       token: "dxcvbanld",
-      orderId: new mongoose.Types.ObjectId().toHexString()
-    }).expect(404)
+      orderId: new mongoose.Types.ObjectId().toHexString(),
+    })
+    .expect(404);
 });
 
-it("Return 401 if order does not exist to the loggedIn User", async ()=>{
-   const order = Order.build({
-     id: new mongoose.Types.ObjectId().toHexString(),
-     price: 10,
-     version: 0,
-     status: OrderStatus.Created,
-     userId: new mongoose.Types.ObjectId().toHexString(),
-   });
-   await order.save();
+it("Return 401 if order does not exist to the loggedIn User", async () => {
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    price: 10,
+    version: 0,
+    status: OrderStatus.Created,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+  });
+  await order.save();
 
-    await request(app).post(url).set("Cookie", global.signin()).send({
+  await request(app)
+    .post(url)
+    .set("Cookie", global.signin())
+    .send({
       token: "dxcvbanld",
-      orderId: order.id
-    }).expect(401)
+      orderId: order.id,
+    })
+    .expect(401);
 });
 
-it("Return 400 when purchasing a cancelled order", async ()=>{
-     const order = Order.build({
-       id: new mongoose.Types.ObjectId().toHexString(),
-       price: 10,
-       version: 0,
-       status: OrderStatus.Cancelled,
-       userId: new mongoose.Types.ObjectId().toHexString(),
-     });
-     await order.save();
+it("Return 400 when purchasing a cancelled order", async () => {
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    price: 10,
+    version: 0,
+    status: OrderStatus.Cancelled,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+  });
+  await order.save();
 
-     await request(app)
-       .post(url)
-       .set("Cookie", global.signin(order.userId))
-       .send({
-         token: "dxcvbanld",
-         orderId: order.id,
-       })
-       .expect(400);
+  await request(app)
+    .post(url)
+    .set("Cookie", global.signin(order.userId))
+    .send({
+      token: "dxcvbanld",
+      orderId: order.id,
+    })
+    .expect(400);
+});
 
-})
-
-it("Returns 201 with valid inputs", async ()=>{
+it("Returns 201 with valid inputs", async () => {
   const price = Math.floor(Math.random() * 100000);
   const order = Order.build({
     id: new mongoose.Types.ObjectId().toHexString(),
@@ -64,24 +72,28 @@ it("Returns 201 with valid inputs", async ()=>{
   });
   await order.save();
 
-   await request(app)
-     .post(url)
-     .set("Cookie", global.signin(order.userId))
-     .send({
-       token: "tok_visa",
-       orderId: order.id,
-     })
-     .expect(201);
-
-     const stripeCharges = await stripe.charges.list({limit: 50});
-     const stripeCharge = stripeCharges.data.find(charge => charge.amount === price*100);
-     expect(stripeCharge).toBeDefined();
-     expect(stripeCharge!.currency).toEqual('usd');
-
-    const payment = await Payment.findOne({
+  await request(app)
+    .post(url)
+    .set("Cookie", global.signin(order.userId))
+    .send({
+      token: "tok_visa",
       orderId: order.id,
-      stripeId: stripeCharge!.id
-    });
+    })
+    .expect(201);
 
-    expect(payment).not.toBeNull();
-})
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+
+  const stripeCharges = await stripe.charges.list({ limit: 50 });
+  const stripeCharge = stripeCharges.data.find(
+    (charge) => charge.amount === price * 100
+  );
+  expect(stripeCharge).toBeDefined();
+  expect(stripeCharge!.currency).toEqual("usd");
+
+  const payment = await Payment.findOne({
+    orderId: order.id,
+    stripeId: stripeCharge!.id,
+  });
+
+  expect(payment).not.toBeNull();
+});
--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -12,6 +12,8 @@ import { body } from "express-validator";
 import { Order } from "../models/order";
 import { stripe } from "../stripe";
 import { Payment } from "../models/payment";
+import { PaymentCreatedPublisher } from "../events/publishers/payment-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -50,8 +52,16 @@ router.post(
       stripeId: charge.id
     });
     await payment.save();
+
+    new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id,
+      orderId: payment.orderId,
+      stripeId: payment.stripeId
+    });
    
-    res.status(201).send({ success: true });
+    res.status(201).send({
+      id: payment.id
+    });
   }
 );
 
