fd66cddeeb32f5620c0f10173585b49fd933a8c6
ppreetii <ppreeti.guptaa@gmail.com>
2023-07-21 03:25:31 +0530

    paying for an order_ client implementation

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,7 +1,20 @@
 import { useEffect, useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
 
-const OrderShow = ({ order }) => {
+import useRequest from "../../hooks/user-request";
+
+const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
+
+  const {doRequest, errors} = useRequest({
+    url : '/api/payments',
+    method: 'post',
+    body: {
+        orderId: order.id,
+    },
+    onSuccess: (payment) => console.log(payment)
+  });
+
   useEffect(() => {
     const findTimeLeft = () => {
       const msLeft = new Date(order.expiresAt) - new Date();
@@ -28,6 +41,13 @@ const OrderShow = ({ order }) => {
     <div>
       <h1>{order.ticket.title}</h1>
       <p>Time left to pay: {timeLeft} seconds</p>
+      <StripeCheckout
+        token={({id}) => doRequest({token : id})}
+        stripeKey="pk_test_51NUzjUHWLWzp1DjSArpzHCHp61bfwVJzZwxMohodOIkvUwerPzhzKk9A4t2YSqUONH4zkRnYMTWvV5CEmIBr0weS00vvzpPALy"
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+      {errors}
     </div>
   );
 };
